07eb75e17563a79ccdec2a1de785f2c0cdafc6b2
U-simba\samu <samu@simba.(none)>
2014-07-16 12:42:40 +0300

    -build

--- gr-gps/build/docs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/samu/koodit/gr/gr-gps/docs
-# Build directory: /Users/samu/koodit/gr/gr-gps/build/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
